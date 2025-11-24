0581a7a869926726b94ec4f82a919881ac670924
EvanMd-Azeltor <151580496+EvanMyDu@users.noreply.github.com>
2025-11-24 16:24:18 +0100

    Structure Regle et Fonctions demandées
    Pas testé avec le main

--- LO21projet/regle.c
+++ LO21projet/regle.c
@@ -3,3 +3,108 @@
 //
 
 #include "regle.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//Convention : SI IL Y A QU'UN SEUL ELEMENT DANS UNE REGLE IL S'AGIT DE LA CONCLUSION
+
+typedef struct ElementRegle {
+    struct ElementRegle *next;
+     char *proposition;
+}ElementRegle;
+
+typedef ElementRegle *Regle;
+
+Regle CreerRegleVide() {
+    return NULL;
+}
+
+Regle AjoutPremisse(Regle r, char *premisse) {
+    ElementRegle *proposition = malloc(sizeof(ElementRegle));
+    proposition->proposition = premisse;
+    proposition->next = NULL;
+    if (r == NULL) {
+        r = proposition;
+        return r;
+    }
+    ElementRegle *p = r;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    p->next = proposition;
+
+    return r;
+}
+
+Regle AjoutConclusion(Regle r, char *conclusion) {
+    return AjoutPremisse(r, conclusion);
+}
+
+int TestPremisse(Regle r, char *proposition) {
+    if (r == NULL || r->next == NULL) {
+        return 1;
+    }
+    else {
+        if (strcmp(r->proposition, proposition) == 0) {
+            return 0;
+        }
+        else {
+            return TestPremisse(r->next, proposition);
+        }
+    }
+}
+
+
+Regle SupprimeProposition(Regle r, char *proposition) {
+    if (r==NULL) {
+        return NULL;
+    }
+    if (r->next != NULL && strcmp(r->proposition, proposition) == 0) {
+        ElementRegle *tmp = r;
+        r=r->next;
+        free(tmp);
+        return r;
+    }
+    else {
+        ElementRegle *p = r;
+        while (p->next->next != NULL) {
+            if (strcmp(p->next->proposition, proposition) == 0) {
+                ElementRegle *tmp = p->next;
+                p->next =p->next->next;
+                free(tmp);
+                return r;
+            }
+            p = p->next;
+        }
+    }
+    return r;
+
+}
+
+int PremisseEstVide(Regle r) {
+    if (r != NULL && r->next != NULL ) {
+        return 0;
+    }
+    return 1;
+}
+
+char* PremierPremisse(Regle r) {
+    if (PremisseEstVide(r) == 0) {
+        return r->proposition;
+    }
+    return NULL;
+
+}
+
+char* Conclusion(Regle r) {
+    if (r == NULL) {
+        return NULL;
+    }
+    ElementRegle *p = r;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    return p->proposition;
+}
--- LO21projet/regle.h
+++ LO21projet/regle.h
@@ -4,5 +4,15 @@
 
 #ifndef UNTITLED1_REGLE_H
 #define UNTITLED1_REGLE_H
+typedef struct ElementRegle ElementRegle;
+typedef ElementRegle *Regle;
 
+Regle CreerRegleVide();
+Regle AjoutPremisse(Regle r, char *premisse);
+Regle AjoutConclusion(Regle r, char *conclusion);
+int TestPremisse(Regle r, char *proposition);
+Regle SupprimeProposition(Regle r, char *proposition);
+int PremisseEstVide(Regle r);
+char* PremierPremisse(Regle r);
+char* Conclusion(Regle r);
 #endif //UNTITLED1_REGLE_H
