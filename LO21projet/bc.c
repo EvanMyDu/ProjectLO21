d1bc55dab6e8edb033510b1c2c2a64a9edeb058d
EvanMd-Azeltor <151580496+EvanMyDu@users.noreply.github.com>
2025-11-26 21:39:09 +0100

    BC Struct + Fonctions
    Pas testé dans le main

--- LO21projet/bc.c
+++ LO21projet/bc.c
@@ -1,5 +1,43 @@
 //
 // Created by Myotte-Duquet on 24/11/2025.
 //
-
+#include <stdlib.h>
+#include <string.h>
 #include "bc.h"
+
+
+typedef struct ElementBC {
+        Regle regle;
+        struct ElementBC *next;
+}ElementBC;
+
+typedef ElementBC *BC;
+
+BC CreerBaseVide() {
+        return NULL;
+}
+
+BC AjoutRegle(BC bc, Regle r) {
+        ElementBC *newel = malloc(sizeof(ElementBC));
+        newel->regle = r;
+        newel->next = NULL;
+        if (bc == NULL) {
+                bc = newel;
+
+                return bc;
+        }
+        ElementBC *p = bc;
+        while (p->next != NULL) {
+                p = p->next;
+        }
+        p->next = newel;
+        return bc;
+}
+
+
+Regle RegleBase(BC bc) {
+        if (bc == NULL) {
+                return NULL;
+        }
+        return bc->regle;
+}
--- LO21projet/bc.h
+++ LO21projet/bc.h
@@ -4,5 +4,12 @@
 
 #ifndef UNTITLED1_BC_H
 #define UNTITLED1_BC_H
+#include "regle.h"
+typedef struct ElementBC ElementBC;
+typedef ElementBC *BC;
+
+BC CreerBaseVide();
+Regle RegleBase(BC bc);
+
 
 #endif //UNTITLED1_BC_H
